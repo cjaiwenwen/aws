fe299e1d18017a49d3c42dd6b0c1b1ac08e218e7
Chen Jun <cjaiwenwen@ChendeMacBook-Air.local>
2018-04-10 16:41:23 +0800

    add everything

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aws',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An AWS Provisioning Tool',
+    long_description=open('README.md').read(),
+    install_requires=['boto3','paramiko','prettytable'],
+    url='https://github.com/cjaiwenwen/aws',
+    author='Chen Jun',
+    author_email='cjaiwenwen@gmail.com'
+)
