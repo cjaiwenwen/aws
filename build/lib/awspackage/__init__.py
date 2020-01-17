2573687a84bb18abe951c8aca52e7820b382b334
Chen Jun <cjaiwenwen@gmail.com>
2020-01-17 17:48:09 +0800

    modify the setup.py

--- build/lib/awspackage/__init__.py
+++ build/lib/awspackage/__init__.py
@@ -1,1 +1,2 @@
-from awspackage import aws
+
+
--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ except ImportError:
 
 setup(
     name='aws_toolkit',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An AWS Provisioning Tool',
     long_description=open('README.md').read(),
-    install_requires=['IPy','boto3','paramiko','prettytable'],
+    long_description_content_type='text/markdown',
+    install_requires=['IPy','boto3','paramiko','prettytable','Ansible'],
     url='https://github.com/cjaiwenwen/aws',
     author='Chen Jun',
     author_email='cjaiwenwen@gmail.com'
